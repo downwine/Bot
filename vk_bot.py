ff83f93692fb2f4f4ff14527aeeadd3a9030e78a
Ksusha <ksenia.ashess@gmail.com>
2021-03-04 01:03:24 +0300

    v1.5.2 filling of relocation_doc

--- vk_bot.py
+++ vk_bot.py
@@ -1,7 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-import vk_api.vk_api
+import vk_api
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.longpoll import VkLongPoll, VkEventType
 
 # API-ключ
 token = "5e57c513cb3a1c9e62eedf59de73e62ec4bc4688d62c1b7508912a146370d929dbef3af9897246fe3c1d3"
@@ -9,6 +10,7 @@ token = "5e57c513cb3a1c9e62eedf59de73e62ec4bc4688d62c1b7508912a146370d929dbef3af
 vk_session = vk_api.VkApi(token=token)
 # Для вызова методов vk_api
 session_api = vk_session.get_api()
+longpoll = VkLongPoll(vk_session)
 
 
 def set_url(website: str):
@@ -22,6 +24,7 @@ def set_url(website: str):
     # print(page.status_code) 200 - удачное подключение
     # Скармливаем bs4 страницу, в переменной soup весь html-код
     soup = BeautifulSoup(page.text, "html.parser")
+
     return soup
 
 
@@ -31,7 +34,8 @@ class VkBot:
     user_id = None
     USERNAME = None
     url = None
-    COMMANDS = ["ПРИВЕТ", "ДАТА", "ВРЕМЯ", "ПОКА", "НАЧАТЬ", "START", "УСТАЛ"]
+    COMMANDS = ["ПРИВЕТ", "ДАТА", "ВРЕМЯ", "ПОКА", "НАЧАТЬ", "START", "УСТАЛ", "ОТПРАВИТЬ ЗАЯВЛЕНИЕ", "ОТПРАВИТЬ ЧЕК",
+                "ЗАЯВЛЕНИЕ НА ВНОС", "ЗАЯВЛЕНИЕ НА ОТЪЕЗД", "ЗАЯВЛЕНИЕ НА ГОСТЯ", "ЗАЯВЛЕНИЕ НА ПЕРЕСЕЛЕНИЕ"]
     city = None
 
     def __init__(self, user_id):
@@ -45,21 +49,108 @@ class VkBot:
             self.city = ", который не указан"
 
     @staticmethod
-    def create_keyboard():
+    def create_default_keyboard():
+        """Создание дефолтной клавиатуры"""
         keyboard = VkKeyboard(one_time=True)
-        keyboard.add_button('Привет', color=VkKeyboardColor.NEGATIVE)
-        keyboard.add_button('Клаватура', color=VkKeyboardColor.POSITIVE)
-        keyboard.add_line()
-        keyboard.add_location_button()
+        keyboard.add_button('Отправить заявление', color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Отправить чек', color=VkKeyboardColor.PRIMARY)
+
         return keyboard.get_keyboard()
 
     @staticmethod
-    def send_msg(self, user_id, message):
-        """Функция для отправки пользователю сообщения"""
+    def create_docs_keyboard():
+        """Создание клавиатуры с выбором документов"""
+        keyboard = VkKeyboard(inline=True)
+        keyboard.add_button('Заявление на внос', color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Заявление на отъезд', color=VkKeyboardColor.PRIMARY)
+        keyboard.add_line()
+        keyboard.add_button('Заявление на гостя', color=VkKeyboardColor.PRIMARY)
+        keyboard.add_button('Заявление на переселение', color=VkKeyboardColor.PRIMARY)
+
+        return keyboard.get_keyboard()
+
+    @staticmethod
+    def send_msg_with_keyboard(self, user_id, message):
+        """Функция для отправки пользователю сообщения с клавиатурой"""
         vk_session.method('messages.send', {'user_id': user_id,
                                             'message': message,
-                                            'keyboard': self.create_keyboard(),
+                                            'keyboard': self.create_default_keyboard(),
                                             'random_id': 0})
+
+    @staticmethod
+    def send_types_of_docs(self, user_id):
+        """Сообщение с клавиатурой документов"""
+        vk_session.method('messages.send', {'user_id': user_id,
+                                            'message': "Выберите тип заявления:",
+                                            'keyboard': self.create_docs_keyboard(),
+                                            'random_id': 0})
+
+    @staticmethod
+    def send_msg_without_keyboard(user_id, message):
+        """Функция для отправки пользователю сообщения без клавиатуры"""
+        vk_session.method('messages.send', {'user_id': user_id,
+                                            'message': message,
+                                            'random_id': 0})
+
+    @staticmethod
+    def create_dictionary(self, doc_id):
+        self.send_msg_without_keyboard(self.user_id,
+                                       "Перед заполнением Вы можете ознакомиться с шаблонами по ссылке "
+                                       "https://vk.com/album-202823499_277877657")
+        answers = []
+        mess = ["Введите свои имя, фамилию и отчество в родительном падеже", "Введите номер комнаты",
+                "Введите контактный телефон", "Введите дату заполнения"]
+        for i in range(3):
+            self.send_msg_without_keyboard(self.user_id, mess[i])
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW:
+                    if event.to_me:
+                        answers.append(event.text)
+                        break
+        dict_of_answers = {}
+
+        if doc_id == 0:
+            print()
+        #elif doc_id == 1:
+
+        #elif doc_id == 2:
+
+        elif doc_id == 3:
+            fields = ["Введите комнату, в которую хотите переселиться",
+                      "Введите комнату, из которой хотите переселиться",
+                      "Введите причину",
+                      "Есть ли у вас академическая задолженность? Да / Нет",
+                      "Имеете ли вы дисциплинарные высказывания? Да / Нет",
+                      "Введите через запятую ФИО соседей, с которыми было согласовано посещение"]
+            dict_keys = ["full_name", "room_number", "phone_number", "room_to", "room_from",
+                         "reason", "academ_debt", "reprimands", "neighbors"]
+            for i in range(len(fields)):
+                self.send_msg_without_keyboard(self.user_id, fields[i])
+                for event in longpoll.listen():
+                    if event.type == VkEventType.MESSAGE_NEW and event.user_id == self.user_id:
+                        if event.to_me:
+                            if i == 5:
+                                string = event.text.split(", ")
+                                answers.append(string)
+                            elif i == 3 or i == 4:
+                                if event.text.upper() == "ДА":
+                                    answers.append(True)
+                                else:
+                                    answers.append(False)
+                            else:
+                                answers.append(event.text)
+                            break
+
+            self.send_msg_without_keyboard(self.user_id, mess[3])
+            for event in longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW and event.user_id == self.user_id:
+                    if event.to_me:
+                        dict_keys.append("current_date")
+                        answers.append(event.text)
+                        break
+            dict_of_answers = dict(zip(dict_keys, answers))
+
+        return dict_of_answers
 
     @staticmethod
     def get_user_name_from_vk_id(user_id):
@@ -90,6 +181,7 @@ class VkBot:
                 if i == j:
                     result += text[j]
                     j += 1
+
         return result
 
     @staticmethod
@@ -108,38 +200,71 @@ class VkBot:
                 result += text[i]
         return result
 
-    def new_message(self, message):
+    def new_message(self, message, user_id):
         """
         Генерация нового сообщения для отправки
+        :param user_id: Идентификатор пользователя
         :param message: Входящее сообщение пользователя
         :return: Сообщение от бота
         """
         # Привет
         if message.upper() in self.COMMANDS[0]:
-            return f"Привет-привет, {self.USERNAME} из города{self.city}!"
+            self.send_msg_with_keyboard(self, user_id,
+                                        f"Привет-привет, {self.USERNAME} из города{self.city}!"
+                                        f" Если я не отвечаю тебе сразу, то не расстраивайся и повтори своё сообщение "
+                                        f"через пару минут")
 
         # Дата
         elif message.upper() == self.COMMANDS[1]:
-            return self.get_date(set_url("https://my-calend.ru/date-and-time-today"))
+            self.send_msg_with_keyboard(self, user_id,
+                                        self.get_date(set_url("https://my-calend.ru/date-and-time-today")))
 
         # Время
         elif message.upper() == self.COMMANDS[2]:
-            return self.get_time(set_url("https://my-calend.ru/date-and-time-today"))
+            self.send_msg_with_keyboard(self, user_id,
+                                        self.get_time(set_url("https://my-calend.ru/date-and-time-today")))
 
         # Пока
         elif message.upper() == self.COMMANDS[3]:
-            return f"Пока-пока, {self.USERNAME}!"
+            self.send_msg_without_keyboard(user_id, f"Пока-пока, {self.USERNAME}!")
 
         # Начало
         elif message.upper() == self.COMMANDS[4] or message.upper() == self.COMMANDS[5]:
-            return f"Введите ваши реальные имя и фамилию"
+            self.send_msg_without_keyboard(user_id, "Введите ваши реальные имя и фамилию")
             # проверка с базой данных и регистрация
 
+        # Устал
         elif message.upper() == self.COMMANDS[6]:
-            vk_session.method('messages.send', {'user_id': self.user_id,
-                                                'attachment': "photo-202823499_457239018",
+            vk_session.method('messages.send', {'user_id': user_id,
+                                                'attachment': "https://sun9-63.userapi.com/impf/4qBJys6hFxf01_"
+                                                           "fbcYhaRifkynsOK7J81Y4e3Q/A70V_KPPhEA.jpg?size=1920x1274&"
+                                                           "quality=96&sign=f493a2a44d93bd9f274f53d110422e10&type=album",
                                                 'random_id': 0})
-            return f"Отдохни, бро"
+
+        # Отправить заявление
+        elif message.upper() == self.COMMANDS[7]:
+            self.send_types_of_docs(self, user_id)
+
+        # Отправить чек
+        elif message.upper() == self.COMMANDS[8]:
+            print()
+
+        # Заявление на внос
+        elif message.upper() == self.COMMANDS[9]:
+            print()
+
+        # Заявление на отъезд
+        elif message.upper() == self.COMMANDS[10]:
+            print()
+
+        # Заявление на гостя
+        elif message.upper() == self.COMMANDS[11]:
+            print()
+
+        # Заявление на переселение
+        elif message.upper() == self.COMMANDS[12]:
+            a = self.create_dictionary(self, 3)
+            print(a)
 
         else:
-            return "Не понимаю о чем вы..."
+            self.send_msg_without_keyboard(user_id, "Не понимаю о чем вы...")
