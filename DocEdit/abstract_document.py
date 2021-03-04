57d7656d68f9519a27a4c856442cb2d0c737d165
Ivan Polukarov <polkarov.i@sch2009.net>
2021-03-04 16:44:15 +0300

    v1.5.4 Abstract Document edit

--- DocEdit/abstract_document.py
+++ DocEdit/abstract_document.py
@@ -1,12 +1,15 @@
 from docxtpl import DocxTemplate
 from docxtpl import RichText
-import os
 
 
 class AbstractDocument:
     def __init__(self):
-        self.template_path = None  #
-        self.document = None
+        if __name__ == "__main__":
+            self.template_path = 'test_path'
+            self.document = DocxTemplate(self.template_path)
+        else:
+            self.template_path = None
+            self.document = None
 
     def write_usual(self, **document_context_kwargs):
         """
@@ -15,9 +18,33 @@ class AbstractDocument:
             :return:
         """
 
-        self.document = DocxTemplate(self.template_path)  # reading document with __init__ path param
+        # self.document = DocxTemplate(self.template_path)  # reading document with __init__ path param
         self.document.render(document_context_kwargs)
         # self.document.save(path_to_save_file)
+
+        return self
+
+    def write_rich(self, text_variable, text_content, render_italic=False,
+                   render_bold=False, render_color='#000000', render_size=14, render_strike=False):
+        """
+            Rich text render
+            :param text_variable:
+            :param text_content
+            :param render_bold: False
+            :param render_italic: False
+            :param render_strike: False
+            :param render_color: '#000000'
+            :param render_size: 14
+            :return:
+        """
+
+        rich_text = RichText(text_content, italic=render_italic, bold=render_bold,
+                             strike=render_strike, color=render_color, size=render_size)
+
+        document_context_kwargs = {text_variable, text_content}
+        self.document.render(document_context_kwargs)
+
+        return self
 
     def save(self, path_to_save_file='DefaultTest.docx'):
         """
@@ -27,13 +54,3 @@ class AbstractDocument:
         """
 
         self.document.save(path_to_save_file)
-
-    def write_rich(self, text_content, italic=False, color='#000000'):
-        """
-            Rich text render
-            :param text_content
-            :param rich_text_kwargs - dict of {{ vars }} and their values
-            :return:
-        """
-
-        # rt = RichText(text_content, *rich_text_kwargs)
