d5c1c2f1c5dc567f9e9e6b432c953f23e93cc96a
Ivan Polukarov <polkarov.i@sch2009.net>
2021-03-10 21:24:40 +0300

    v1.5.8 full_name_processing and full_name_check added

--- DocEdit/regular_expressions.py
+++ DocEdit/regular_expressions.py
@@ -23,3 +23,53 @@ def mobile_valid_check(phone_number):
     return True if pattern.search(phone_number) else False
 
 
+def reformat_mobile(phone_number):
+    """
+        valid phone check
+        :param: phone_number: phone_number
+        :return: processed phone number with format xxx.xxx.xx.xx
+    """
+
+    if not mobile_valid_check(phone_number):
+        return None
+
+    phone_number = phone_number.replace(' ', '')
+    phone_number = phone_number.replace('-', '')
+    phone_number = phone_number.replace(')', '')
+    phone_number = phone_number.replace('(', '')
+    phone_number = phone_number.replace('+', '')
+
+    print(phone_number)
+
+
+def full_name_check(full_name):
+    """
+        valid phone check
+        :param: full_name: string with surname, name, (patronymic)
+        :return: bool: is full_name valid
+    """
+
+    full_name = full_name.lstrip(' ')
+    full_name = full_name.rstrip(' ')
+
+    pattern = re.compile(r'[^a-zA-Zа-яА-Я ]')
+
+    if pattern.search(full_name):
+        return False
+    return len(full_name.split(' ')) >= 2
+
+
+def full_name_processing(full_name):
+    """
+        valid phone check
+        :param: full_name: string with surname, name, (patronymic)
+        :return: None if not full_name_check else processed full_name
+    """
+
+    if not full_name_check(full_name):
+        return None
+
+    full_name = full_name.lower()
+    full_name = full_name.title()
+
+    return full_name
