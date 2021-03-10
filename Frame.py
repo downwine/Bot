1675fb0e80e27e69efeb2f250eb0c36e6e54d0ef
Ksusha <ksenia.ashess@gmail.com>
2021-03-10 21:48:44 +0300

    v1.6.8 improve the source code

--- Frame.py
+++ Frame.py
@@ -1,9 +1,18 @@
 from vk_api.longpoll import VkLongPoll, VkEventType
-from vk_bot import VkBot, token, vk_session, longpoll
+from vk_bot import VkBot, token, vk_session, longpoll, send_msg_without_keyboard
+import time
 
 # Основной цикл
 print("Server started")
 # Слушаем сервер
+"""
+start_time = time.time()
+end_time = time.time()
+        if end_time - start_time > 5.0:
+            send_msg_without_keyboard(event.id, "Вы отвечали слишком долго, я не дождался, повторите запрос ещё раз")
+            break
+"""
+
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW:  # Если новое сообщение
         if event.to_me:  # Для бота
--- vk_bot.py
+++ vk_bot.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import vk_api
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll, VkEventType
+import time
+import re
 
 # API-ключ
 token = "5e57c513cb3a1c9e62eedf59de73e62ec4bc4688d62c1b7508912a146370d929dbef3af9897246fe3c1d3"
@@ -11,6 +13,27 @@ vk_session = vk_api.VkApi(token=token)
 # Для вызова методов vk_api
 session_api = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
+
+
+def mobile_valid_check(phone_number):
+    """
+    valid phone check
+    :param: phone_number: phone_number
+    :return: bool: is number valid
+    """
+
+    pattern = re.compile(r'(^[+0-9]{1,3})*([0-9]{10,11}$)')
+    letters = re.compile(r'[^0-9-+() ]')
+
+    if letters.search(phone_number):
+        return False
+
+    phone_number = phone_number.replace(' ', '')
+    phone_number = phone_number.replace('-', '')
+    phone_number = phone_number.replace(')', '')
+    phone_number = phone_number.replace('(', '')
+
+    return True if pattern.search(phone_number) else False
 
 
 def set_url(website: str):
@@ -86,34 +109,153 @@ class VkBot:
                                             'random_id': 0})
 
     @staticmethod
-    def send_msg_without_keyboard(user_id, message):
-        """Функция для отправки пользователю сообщения без клавиатуры"""
-        vk_session.method('messages.send', {'user_id': user_id,
-                                            'message': message,
-                                            'random_id': 0})
+    def get_current_date(self, mess, dict_keys, answers):
+        send_msg_without_keyboard(self.user_id, mess[3])
+        for event in longpoll.listen():
+            if event.type == VkEventType.MESSAGE_NEW and event.user_id == self.user_id:
+                if event.to_me:
+                    if len(event.text) != 8:
+                        send_msg_without_keyboard(self.user_id,
+                                                  "Дата введена некорректно, повторите ввод")
+                        continue
+                    dict_keys.append("current_date")
+                    answers.append(event.text)
+                    break
+        dict_of_answers = dict(zip(dict_keys, answers))
+
+        return dict_of_answers
 
     @staticmethod
     def create_dictionary(self, doc_id):
-        self.send_msg_without_keyboard(self.user_id,
-                                       "Перед заполнением Вы можете ознакомиться с шаблонами по ссылке "
-                                       "https://vk.com/album-202823499_277877657")
+        send_msg_without_keyboard(self.user_id,
+                                  "Перед заполнением Вы можете ознакомиться с шаблонами по ссылке "
+                                  "https://vk.com/album-202823499_277877657")
         answers = []
-        mess = ["Введите свои имя, фамилию и отчество в родительном падеже", "Введите номер комнаты",
-                "Введите контактный телефон", "Введите дату заполнения"]
+        mess = ["Введите свои имя, фамилию и отчество в родительном падеже",
+                "Введите номер комнаты, в которой проживаете",
+                "Введите контактный телефон",
+                "Введите дату заполнения в формате дд.мм.гг"]
+
         for i in range(3):
-            self.send_msg_without_keyboard(self.user_id, mess[i])
+            send_msg_without_keyboard(self.user_id, mess[i])
             for event in longpoll.listen():
                 if event.type == VkEventType.MESSAGE_NEW:
                     if event.to_me:
+                        if i == 0:
+                            if len(event.text.split(" ")) < 2:
+                                send_msg_without_keyboard(self.user_id, "ФИО введены некорректно, повторите ввод")
+                                continue
+                        if i == 1:
+                            if 201 > int(event.text) or int(event.text) > 918:
+                                send_msg_without_keyboard(self.user_id,
+                                                          "Номер комнаты введён некорректно, повторите ввод")
+                                continue
+                        if i == 2:
+                            if not mobile_valid_check(event.text):
+                                send_msg_without_keyboard(self.user_id,
+                                                          "Номер телефона введён некорректно, повторите ввод")
+                                continue
+
                         answers.append(event.text)
                         break
-        dict_of_answers = {}
 
         if doc_id == 0:
-            print()
-        #elif doc_id == 1:
+            fields = ["Введите дату, когда хотите внести/вынести вещи, в формате дд.мм.гг",
+                      "Заявление на внос или на вынос?",
+                      "Перечислите через запятую вещи, которые хотите внести/вынести",
+                      "Введите через запятую соседей, с которыми согласован внос/вынос"]
+            dict_keys = ["full_name", "room_number", "phone_number", "date_of_moving", "in_or_out",
+                         "list_of_items", "neighbors"]
 
-        #elif doc_id == 2:
+            for i in range(len(fields)):
+                send_msg_without_keyboard(self.user_id, fields[i])
+                for event in longpoll.listen():
+                    if event.type == VkEventType.MESSAGE_NEW and event.user_id == self.user_id:
+                        if event.to_me:
+                            if i == 0:
+                                if len(event.text) != 8:
+                                    send_msg_without_keyboard(self.user_id,
+                                                              "Дата введена некорректно, повторите ввод")
+                                    continue
+                            if i == 1:
+                                if event.text.upper() != "ВНОС" or event.text.upper() != "ВЫНОС":
+                                    send_msg_without_keyboard(self.user_id,
+                                                              "Ввод некорректен, повторите ввод")
+                                    continue
+                            if i == 3 or i == 2:
+                                string = event.text.split(", ")
+                                answers.append(string)
+                            else:
+                                answers.append(event.text)
+                            break
+
+            return self.get_current_date(self, mess, dict_keys, answers)
+
+        elif doc_id == 1:
+            fields = ["Введите период, начиная с которого Вы будете отсутствовать, в формате дд.мм.гг",
+                      "Введите период, по который вы будете отсутствовать, в формате дд.мм.гг",
+                      "Введите причину отсутствия"]
+            dict_keys = ["full_name", "room_number", "phone_number", "period_from", "period_to",
+                         "reason"]
+
+            for i in range(len(fields)):
+                send_msg_without_keyboard(self.user_id, fields[i])
+                for event in longpoll.listen():
+                    if event.type == VkEventType.MESSAGE_NEW and event.user_id == self.user_id:
+                        if event.to_me:
+                            if i == 0 or i == 1:
+                                if len(event.text) != 8:
+                                    send_msg_without_keyboard(self.user_id,
+                                                              "Дата введена некорректно, повторите ввод")
+                                    continue
+                            answers.append(event.text)
+                            break
+
+            return self.get_current_date(self, mess, dict_keys, answers)
+
+        elif doc_id == 2:
+            fields = ["Введите ФИО гостя, которого приглашаете",
+                      "Введите номер комнаты, в которую приглашаете",
+                      "Введите дату, в которую хотите пригласить гостя, в формате дд.мм.гг",
+                      "Введите время, начиная с которого гость будет присутствовать",
+                      "Введите время, по которое гость будет присутствовать",
+                      "Введите через запятую ФИО соседей, с которыми согласован проход гостя"]
+            dict_keys = ["full_name", "room_number", "phone_number", "guest_name", "invitation_room",
+                         "day_of_visit", "time_from", "time_to", "neighbors"]
+
+            for i in range(len(fields)):
+                send_msg_without_keyboard(self.user_id, fields[i])
+                for event in longpoll.listen():
+                    if event.type == VkEventType.MESSAGE_NEW and event.user_id == self.user_id:
+                        if event.to_me:
+                            if i == 0:
+                                if len(event.text.split(" ")) < 2:
+                                    send_msg_without_keyboard(self.user_id,
+                                                              "ФИО введены некорректно, повторите ввод")
+                                    continue
+                            if i == 1:
+                                if 201 > int(event.text) or int(event.text) > 918:
+                                    send_msg_without_keyboard(self.user_id,
+                                                              "Номер комнаты введён некорректно, повторите ввод")
+                                    continue
+                            if i == 2:
+                                if len(event.text) != 8:
+                                    send_msg_without_keyboard(self.user_id,
+                                                              "Дата введена некорректно, повторите ввод")
+                                    continue
+                            if i == 3 or i == 4:
+                                if not event.text.isdigit():
+                                    send_msg_without_keyboard(self.user_id,
+                                                              "Время введено некорректно, повторите ввод")
+                                    continue
+                            if i == 5:
+                                string = event.text.split(", ")
+                                answers.append(string)
+                            else:
+                                answers.append(event.text)
+                            break
+
+            return self.get_current_date(self, mess, dict_keys, answers)
 
         elif doc_id == 3:
             fields = ["Введите комнату, в которую хотите переселиться",
@@ -124,33 +266,34 @@ class VkBot:
                       "Введите через запятую ФИО соседей, с которыми было согласовано посещение"]
             dict_keys = ["full_name", "room_number", "phone_number", "room_to", "room_from",
                          "reason", "academ_debt", "reprimands", "neighbors"]
+
             for i in range(len(fields)):
-                self.send_msg_without_keyboard(self.user_id, fields[i])
+                send_msg_without_keyboard(self.user_id, fields[i])
                 for event in longpoll.listen():
                     if event.type == VkEventType.MESSAGE_NEW and event.user_id == self.user_id:
                         if event.to_me:
+                            if i == 0 or i == 1:
+                                if 201 > int(event.text) or int(event.text) > 1000:
+                                    send_msg_without_keyboard(self.user_id,
+                                                              "Номер комнаты введён некорректно, повторите ввод")
+                                    continue
+                            if i == 3 or i == 4:
+                                if event.text.upper() == "ДА":
+                                    answers.append(True)
+                                elif event.text.upper() == "НЕТ":
+                                    answers.append(False)
+                                else:
+                                    send_msg_without_keyboard(self.user_id,
+                                                              "Ввод некорректен, повторите ввод")
+                                    continue
                             if i == 5:
                                 string = event.text.split(", ")
                                 answers.append(string)
-                            elif i == 3 or i == 4:
-                                if event.text.upper() == "ДА":
-                                    answers.append(True)
-                                else:
-                                    answers.append(False)
                             else:
                                 answers.append(event.text)
                             break
 
-            self.send_msg_without_keyboard(self.user_id, mess[3])
-            for event in longpoll.listen():
-                if event.type == VkEventType.MESSAGE_NEW and event.user_id == self.user_id:
-                    if event.to_me:
-                        dict_keys.append("current_date")
-                        answers.append(event.text)
-                        break
-            dict_of_answers = dict(zip(dict_keys, answers))
-
-        return dict_of_answers
+            return self.get_current_date(self, mess, dict_keys, answers)
 
     @staticmethod
     def get_user_name_from_vk_id(user_id):
@@ -226,19 +369,19 @@ class VkBot:
 
         # Пока
         elif message.upper() == self.COMMANDS[3]:
-            self.send_msg_without_keyboard(user_id, f"Пока-пока, {self.USERNAME}!")
+            send_msg_without_keyboard(user_id, f"Пока-пока, {self.USERNAME}!")
 
         # Начало
         elif message.upper() == self.COMMANDS[4] or message.upper() == self.COMMANDS[5]:
-            self.send_msg_without_keyboard(user_id, "Введите ваши реальные имя и фамилию")
+            send_msg_without_keyboard(user_id, "Введите ваши реальные имя и фамилию")
             # проверка с базой данных и регистрация
 
         # Устал
         elif message.upper() == self.COMMANDS[6]:
             vk_session.method('messages.send', {'user_id': user_id,
                                                 'attachment': "https://sun9-63.userapi.com/impf/4qBJys6hFxf01_"
-                                                           "fbcYhaRifkynsOK7J81Y4e3Q/A70V_KPPhEA.jpg?size=1920x1274&"
-                                                           "quality=96&sign=f493a2a44d93bd9f274f53d110422e10&type=album",
+                                                              "fbcYhaRifkynsOK7J81Y4e3Q/A70V_KPPhEA.jpg?size=1920x1274&"
+                                                              "quality=96&sign=f493a2a44d93bd9f274f53d110422e10&type=album",
                                                 'random_id': 0})
 
         # Отправить заявление
@@ -251,15 +394,18 @@ class VkBot:
 
         # Заявление на внос
         elif message.upper() == self.COMMANDS[9]:
-            print()
+            a = self.create_dictionary(self, 0)
+            print(a)
 
         # Заявление на отъезд
         elif message.upper() == self.COMMANDS[10]:
-            print()
+            a = self.create_dictionary(self, 1)
+            print(a)
 
         # Заявление на гостя
         elif message.upper() == self.COMMANDS[11]:
-            print()
+            a = self.create_dictionary(self, 2)
+            print(a)
 
         # Заявление на переселение
         elif message.upper() == self.COMMANDS[12]:
@@ -267,4 +413,11 @@ class VkBot:
             print(a)
 
         else:
-            self.send_msg_without_keyboard(user_id, "Не понимаю о чем вы...")
+            send_msg_without_keyboard(user_id, "Не понимаю о чем вы...")
+
+
+def send_msg_without_keyboard(user_id, message):
+    """Функция для отправки пользователю сообщения без клавиатуры"""
+    vk_session.method('messages.send', {'user_id': user_id,
+                                        'message': message,
+                                        'random_id': 0})
